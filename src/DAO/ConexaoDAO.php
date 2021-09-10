d1fcebfdfce6cf9260bef808283c6b8fca9cf1e5
joaoortiz <48797718+joaoortiz@users.noreply.github.com>
2021-09-10 15:14:04 -0300

    Update ConexaoDAO.php

--- src/DAO/ConexaoDAO.php
+++ src/DAO/ConexaoDAO.php
@@ -7,13 +7,12 @@ class ConexaoDAO {
     public function abrirConexao (){
          $vConn = NULL; 
         try{
-            $server = "mysql:host=localhost; dbname=BDTecJobs";
-            $user = "root";
-            $pass = "";
+            $server = "mysql:host=mysql.esrc.com.br; dbname=esrc10";
+            $user = "esrc10";
+            $pass = "projeto10";
             $vConn = new PDO($server, $user, $pass);
             $vConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $vConn->exec("set names utf8");
-                    
         } catch (Exception $ex) {
             echo "ERRO: Falha na conexão ao Banco de dados.<br>";
             echo "Descrição: " . $ex->getMessage();
